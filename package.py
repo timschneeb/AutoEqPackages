ac582ec7160a2baf0f89c17b2c2b378f3dbc21cc
Tim Schneeberger <tim.schneeberger@outlook.de>
2021-12-08 21:46:53 +0100

    Update package.py

--- package.py
+++ package.py
@@ -90,7 +90,7 @@ def main():
         "commit": subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('ascii').strip(),
         "commit_time": datetime.utcfromtimestamp(int(subprocess.check_output(['git', 'log', '-1', '--format=%at'], text=True).strip())).strftime('%Y/%m/%d %H:%M:%S'),
         "package_time": time.strftime('%Y/%m/%d %H:%M:%S'),
-        "package_url": "https://github.com/ThePBone/AutoEqPackages/blob/main/archive.tar.gz",
+        "package_url": "https://github.com/ThePBone/AutoEqPackages/raw/main/archive.tar.gz",
         "type": ["GraphicEQ", "CSV"]
     }]
     with open('export/version.json', 'w') as outfile:
