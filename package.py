170a0c7b92a35cdec661b1063ea2e67279df71c9
Tim Schneeberger <tim.schneeberger@outlook.de>
2021-12-08 21:37:13 +0100

    Update package.py

--- package.py
+++ package.py
@@ -86,13 +86,13 @@ def main():
         json.dump(table, outfile)
 
     # Create version.json
-    entry = {
+    entry = [{
         "commit": subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('ascii').strip(),
         "commit_time": datetime.utcfromtimestamp(int(subprocess.check_output(['git', 'log', '-1', '--format=%at'], text=True).strip())).strftime('%Y/%m/%d %H:%M:%S'),
         "package_time": time.strftime('%Y/%m/%d %H:%M:%S'),
         "package_url": "https://github.com/ThePBone/AutoEqPackages/blob/main/archive.tar.gz",
         "flavor": "GraphicEQ+CSV"
-    }
+    }]
     with open('export/version.json', 'w') as outfile:
         json.dump(entry, outfile)
 
